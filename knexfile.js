ea505d2383a44698db6e9f632e2c58ba208772c1
David Quiñones <daquinons@users.noreply.github.com>
2019-07-18 13:25:03 +0200

    Create Ingredients table

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,12 @@ module.exports = {
     connection: {
       filename: './data/database.db3'
     },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds : {
+      directory: './data/seeds'
+    },
     // needed when using foreign keys
     pool: {
       afterCreate: (conn, done) => {
